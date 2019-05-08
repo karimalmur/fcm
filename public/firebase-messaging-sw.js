8835bd0fa91f211eab1e2287bb49578a3866e855
Karimit <karimit@live.com>
2019-05-08 07:11:55 +0200

    Updates

--- public/firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: "695744899438"
-});
-
-const messaging = firebase.messaging();
