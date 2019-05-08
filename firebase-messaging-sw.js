f1af12a0a71e79da79154d05770ab26ba4a25185
Karimit <karimit@live.com>
2019-05-08 05:21:49 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
